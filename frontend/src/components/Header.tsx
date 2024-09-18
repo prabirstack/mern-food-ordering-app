b49a650e848c4ba1e0f1add11de7d8a739db41a7
Prabir <prabirsacademy@gmail.com>
2024-09-19 03:03:02 +0530

    layout and header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+import logoImg from "../assets/logo.svg"
+const Header = () => {
+  return (
+    <div className="border-b-2 border-b-red-500 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-3xl font-bold tracking-tight text-red-500">
+          <img src={logoImg} alt="" />
+        </Link>
+      </div>
+    </div>
+  )
+}
+export default Header
